6a6681d6b16e0ebe5b11bb0b42492d4cf045f727
AndrewDRichter <andrewrichter1@hotmail.com>
2024-12-06 17:15:39 -0300

    testing middleware features

--- cadastros/middleware.py
+++ cadastros/middleware.py
@@ -4,7 +4,13 @@ class GetTenant:
         self.get_response = get_response
 
     def __call__(self, request):
-        print(request)
+        for k, v in request.META.items():
+            print(f"{k}: {v}")
+        print('----')
+        print(request.META["QUERY_STRING"])
+        request.META["QUERY_STRING"] = "/homepage"
+        print('----')
+        print(request.META["QUERY_STRING"])
         response = self.get_response(request)
         print('-----**------**-----')
         print(response)
