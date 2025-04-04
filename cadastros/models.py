9685b3654b94ac8a721bb730d45732a33024bb89
AndrewDRichter <andrewrichter1@hotmail.com>
2025-04-04 17:06:42 -0300

    initial commit

--- cadastros/models.py
+++ cadastros/models.py
@@ -1,8 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
 
 
 class Tenant(models.Model):
-    name = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='tenants')
+    name = models.CharField(unique=True, max_length=200)
 
-class BaseModel(models.Model):
-    ...
+    def __str__(self):
+        return self.name
+
+
+class BaseTenantClass(models.Model):
+    tenant = models.ForeignKey(Tenant, on_delete=models.PROTECT)
+
+
+class Entity(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django.http.response import HttpResponseBadRequest
+from django.contrib.auth.decorators import login_required
+from .models import Tenant
+from django.contrib.auth.models import User
 
 # Create your views here.
-
+@login_required
 def my_home(request):
-    print("-------view init------")
-    # print(request.META)
-    print("-------view end------")
-    # print(request.COOKIES)
-    print(request.get_host())
-    return HttpResponse("<h1>Test view</h1>")
+    tenancies = request.user.tenants.all()
+    print(tenancies)
+    url_tenant = request.get_host().split('.')[0]
+    if request.user.username == url_tenant:
+        # print(request.COOKIES)
+        # print(request.get_host())
+        return HttpResponse("<h1>Test view</h1>")
+    else:
+        return HttpResponseBadRequest('<h1>You are not allowed to see this.</h1>')
